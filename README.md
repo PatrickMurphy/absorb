3ddf9fedb963c9e8d3dfe169a555dd782d989870
PatrickMurphy <patrick@patrickmurphyphoto.com>
2015-07-02 10:39:24 -0700

    Allow option to only merge if object 1 does not contain key

--- README.md
+++ README.md
@@ -19,22 +19,29 @@ A recursive deep object merge utility function. ObjectParam1 absorbs properties 
 	
 	console.log(obj1); // Output: { foo: 123, bar: 123, key: 'value' }
 
-  You can use the optional third parameter set to true to preserve the original object, for example to make a clone.
+You can use the optional third parameter set to `true` to make a clone.
 	
 	obj1 = { foo: 123, bar: 456 };
 	obj3 = absorb(obj1, obj2, true);
 	
 	console.log(obj1); // Output: { foo: 123, bar: 456 }
 	console.log(obj3); // Output: { foo: 123, bar: 123, key: 'value' }
+	
+You can use the optional fourth parameter set to `true` to only add properties that are not already set in the destination object. Note: This will not overwrite sub-properties in deep objects, however it will add new ones
+	
+	obj1 = { person: {name: 'Patrick', lastname: 'Murphy'}, career: 'Software Engineer'};
+	obj2 = { person: {name: 'Jered', height: '6`1'}, career: 'Investment Banker', hobbies: 'photography'};
+	absorb(obj1, obj2, false, true);
+	
+	console.log(obj1); // Output: { person: {name: 'Patrick', lastname: 'Murphy', height: '6`1'}, career: 'Software Engineer', hobbies: 'photography'};
 
 ## Contributing
 
 In lieu of a formal styleguide, take care to maintain the existing coding style.
 Add unit tests for any new or changed functionality. Lint and test your code.
 
-## Planned Features
-Allow option to only merge if object 1 does not contain key
 
 ## Release History
 
+* 1.0.0 Add PreserveObject paramete, allowing option to only merge if object 1 does not contain key
 * 0.1.0 Initial release
--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 var isObject = require('is-object');
 
-var absorb = function (destObject, sourceObject, preserveOriginal) {
-	// default value
-	preserveOriginalObject = preserveOriginal || false;
-	
+var absorb = function (destObject, sourceObject, clone, preserveOriginal) {
+	// default values
+	clone = clone || false;
+	preserveOriginal = preserveOriginal || false;
+
 	var index,
-			tempObj = destObject;
+		tempObj = destObject;
 
 	for (index in sourceObject) { // for each property in source
-			if (sourceObject.hasOwnProperty(index)) { // if property is not part of prototype
-					if (isObject(sourceObject[index])) {
-							// if property is equal to an object, check too
-							if (tempObj.hasOwnProperty(index)) {
-									// Dest also has this object, let's merge!
-									absorb(tempObj[index], sourceObject[index]);
-							} else {
-									// Dest does not have this object, let's copy the whole object over!
-									tempObj[index] = sourceObject[index];
-							}
-					} else {
-							// Property does not contain an object, let's copy over the data
-							tempObj[index] = sourceObject[index]; // overwrite previous value or add new
-					}
+		if (sourceObject.hasOwnProperty(index)) { // if property is not part of prototype
+			if (isObject(sourceObject[index])) {
+				// if property is equal to an object, check too
+				if (tempObj.hasOwnProperty(index)) {
+					// Dest also has this object, let's merge!
+					absorb(tempObj[index], sourceObject[index]);
+				} else {
+					// Dest does not have this object, let's copy the whole object over!
+					tempObj[index] = sourceObject[index];
+				}
+			} else {
+				// Property does not contain an object, let's copy over the data
+				if(!(preserveOriginal && destObject.hasOwnProperty(index))){
+					// overwrite previous value or add new if preserve original
+					tempObj[index] = sourceObject[index];
+				}
 			}
+		}
 	}
-		
-	if(!preserveOriginal) {
+
+	if (!clone) {
 		destObject = tempObj;
 	}
 	return tempObj;
